09ff91543e204b3eadf92f52d3f3513424bdb689
Facundo Javier Gelatti <javiergelatti@gmail.com>
2020-04-12 22:48:33 -0300

    Usar objetos de dominio para reificar los puños y la espada como arma

--- lib/guerrero.rb
+++ lib/guerrero.rb
@@ -2,11 +2,11 @@ class Guerrero
   attr_reader :vida
   attr_reader :defensa
 
-  def initialize(fuerza:, defensa:, vida:, coeficiente_espada: 1)
+  def initialize(fuerza:, defensa:, vida:, arma: Puños.new)
     @fuerza = fuerza
     @defensa = defensa
     @vida = vida
-    @coeficiente_espada = coeficiente_espada
+    @arma = arma
   end
 
   def atacar_a(otra_unidad)
@@ -16,11 +16,25 @@ class Guerrero
   end
 
   def ataque
-    @fuerza * @coeficiente_espada
+    @fuerza * @arma.coeficiente_ataque
   end
 
   def recibir_daño(cantidad_de_daño)
     daño_final = (cantidad_de_daño - @defensa).clamp(0, @vida)
     @vida -= daño_final
+  end
+end
+
+class Puños
+  def coeficiente_ataque
+    1
+  end
+end
+
+class Espada
+  attr_reader :coeficiente_ataque
+
+  def initialize(coeficiente_ataque)
+    @coeficiente_ataque = coeficiente_ataque
   end
 end
