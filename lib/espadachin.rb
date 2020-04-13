02c128bfc4a3bf2fcdefc321024307d558ab23b2
Facundo Javier Gelatti <javiergelatti@gmail.com>
2020-04-12 22:30:12 -0300

    Extraer método ataque

--- lib/espadachin.rb
+++ lib/espadachin.rb
@@ -12,7 +12,11 @@ class Espadachin
   def atacar_a(otra_unidad)
     raise "una unidad no puede atacarse a sí misma" if self == otra_unidad
 
-    otra_unidad.recibir_daño(@fuerza * @coeficiente_espada)
+    otra_unidad.recibir_daño(ataque)
+  end
+
+  def ataque
+    @fuerza * @coeficiente_espada
   end
 
   def recibir_daño(cantidad_de_daño)
--- lib/guerrero.rb
+++ lib/guerrero.rb
@@ -11,7 +11,11 @@ class Guerrero
   def atacar_a(otra_unidad)
     raise "una unidad no puede atacarse a sí misma" if self == otra_unidad
 
-    otra_unidad.recibir_daño(@fuerza)
+    otra_unidad.recibir_daño(ataque)
+  end
+
+  def ataque
+    @fuerza
   end
 
   def recibir_daño(cantidad_de_daño)
