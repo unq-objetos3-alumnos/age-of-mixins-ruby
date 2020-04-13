2f0bb494b45742a75f837283dc8e53c88bd8395e
Facundo Javier Gelatti <javiergelatti@gmail.com>
2020-04-12 22:39:47 -0300

    Reificar la espada del Espadachin

--- lib/espadachin.rb
+++ lib/espadachin.rb
@@ -1,12 +1,20 @@
 require "guerrero"
 
 class Espadachin < Guerrero
-  def initialize(coeficiente_espada:, **args)
+  def initialize(espada:, **args)
     super(**args)
-    @coeficiente_espada = coeficiente_espada
+    @espada = espada
   end
 
   def ataque
-    super * @coeficiente_espada
+    super * @espada.coeficiente_ataque
+  end
+end
+
+class Espada
+  attr_reader :coeficiente_ataque
+
+  def initialize(coeficiente_ataque)
+    @coeficiente_ataque = coeficiente_ataque
   end
 end
--- spec/espadachin_spec.rb
+++ spec/espadachin_spec.rb
@@ -38,6 +38,6 @@ describe "espadachines" do
   end
 
   def un_espadachin_con(fuerza: 0, defensa: 0, vida: 100, coeficiente_espada: 1)
-    Espadachin.new(fuerza: fuerza, defensa: defensa, vida: vida, coeficiente_espada: coeficiente_espada)
+    Espadachin.new(fuerza: fuerza, defensa: defensa, vida: vida, espada: Espada.new(coeficiente_espada))
   end
 end
